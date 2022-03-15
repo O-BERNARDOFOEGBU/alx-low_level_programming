739528379c8289da08e787eba0ff6ae41dbddff2
obinnaofoegbu71@gmail.com <obinnaofoegbu71@gmail.com>
2022-03-16 00:36:28 +0100

    cor

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,36 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-* main - print the word "Holberton"
-* Return: Always 0 (Success)
-*/
+ * main - print _putchar, followed by new line
+ * Return: 0
+ */
+
 int main(void)
 {
-int i;
-char p[] = "Holberton";
-for (i = 0; i < 9; i++)
-{
-_putchar(p[i]);
-}
-_putchar('\n');
-return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
